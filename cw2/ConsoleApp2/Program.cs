ff902d85c0183a635e8191057c9735624054a8bb
Maksym Holubyev <s28216@pjwstk.edu.pl>
2024-03-08 15:09:41 +0100

    GetAvg

--- cw2/ConsoleApp2/Program.cs
+++ cw2/ConsoleApp2/Program.cs
@@ -6,3 +6,15 @@ Console.WriteLine(myint);
 
 string mystr = Console.ReadLine();
 Console.WriteLine(mystr);
+Console.WriteLine(GetAwg([1,2,3,4,5,6]));
+
+static double GetAwg(int[] arr)
+{
+    double sum = 0;
+    foreach (int var in arr)
+    {
+        sum += var;
+    }
+
+    return sum / arr.Length;
+}
