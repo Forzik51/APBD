52dbbaf86c637b4172f73bfe5cd8816e339a18f9
unknown <supermaxxbox@gmail.com>
2024-03-30 00:40:39 +0200

    ZAD-3

---
+++ ZAD-3/zadanie/LegacyApp.Tests/UserServiceTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using JetBrains.Annotations;
+using LegacyApp;
+using Xunit;
+
+namespace LegacyApp.Tests;
+
+[TestSubject(typeof(UserService))]
+public class UserServiceTest
+{
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_FirstName_Is_Missing()
+    {
+        //Arrange - zaleznosz do testu
+        var userService = new UserService();
+        //Act - test
+        var addResult = userService.AddUser("", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+        //Assert - wynik
+        Assert.False(addResult);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_LastName_Is_Missing()
+    {
+        //Arrange - zaleznosz do testu
+        var userService = new UserService();
+        //Act - test
+        var addResult = userService.AddUser("John", "", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+        //Assert - wynik
+        Assert.False(addResult);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_Email_Not_Valid()
+    {
+        //Arrange - zaleznosz do testu
+        var userService = new UserService();
+        //Act - test
+        var addResult = userService.AddUser("John", "", "johndoegmailcom", DateTime.Parse("1982-03-21"), 1);
+        //Assert - wynik
+        Assert.False(addResult);
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_User_Age_Less_Then_Allowed()
+    {
+        //Arrange - zaleznosz do testu
+        var userService = new UserService();
+        //Act - test
+        var addResult = userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("2005-04-21"), 1);
+        //Assert - wynik
+        Assert.False(addResult);
+        
+    }
+    
+    [Fact]
+    public void AddUser_Should_Throw_Argument_Exception_When_Client_Doesnt_Exit()
+    {
+        //Arrange - zaleznosz do testu
+        var userService = new UserService();
+        //Act & Assert
+        Assert.Throws<ArgumentException>(() =>
+        {
+            userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 0);
+        });
+    }
+    
+    [Fact]
+    public void AddUser_Should_Throw_Argument_Exception_When_Client_LastName_Incorrect()
+    {
+        //Arrange - zaleznosz do testu
+        var userService = new UserService();
+        //Act & Assert
+        Assert.Throws<ArgumentException>(() =>
+        {
+            userService.AddUser("John", "Alonso", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 3);
+        });
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_False_When_User_Has_Credit_Limit_Less_Than_Five_Hundred()
+    {
+        //Arrange - zaleznosz do testu
+        var userService = new UserService();
+        //Act - test
+        var addResult = userService.AddUser("John", "Kowalski", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);
+        //Assert - wynik
+        Assert.False(addResult);
+        
+    }
+    
+    [Fact]
+    public void AddUser_Should_Return_True_When_All_User_Data_Is_Correct()
+    {
+        //Arrange - zaleznosz do testu
+        var userService = new UserService();
+        //Act - test
+        var addResult = userService.AddUser("John", "Doe", "johndoe@gmail.com", DateTime.Parse("1982-03-21"), 1);        //Assert - wynik
+        Assert.True(addResult);
+        
+    }
+    
+}
---
+++ ZAD-3/zadanie/LegacyApp/UserService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (IsFirstNameValid(firstName) || IsLastNameValid(lastName))
+            {
+                return false;
+            }
+
+            if (IsEmailValid(email))
+            {
+                return false;
+            }
+
+            var age = CalculateAgeUsingBirthday(dateOfBirth);
+
+            if (UserAgeLessThenAllowed(age))
+            {
+                return false;
+            }
+
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientId);
+
+            var user = new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else if (client.Type == "ImportantClient")
+            {
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    creditLimit = creditLimit * 2;
+                    user.CreditLimit = creditLimit;
+                }
+            }
+            else
+            {
+                user.HasCreditLimit = true;
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    user.CreditLimit = creditLimit;
+                }
+            }
+
+            if (UserHasCreditLimitLessThanFiveHundred(user))
+            {
+                return false;
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private static bool UserAgeLessThenAllowed(int age)
+        {
+            return age < 21;
+        }
+
+        private static bool UserHasCreditLimitLessThanFiveHundred(User user)
+        {
+            return user.HasCreditLimit && user.CreditLimit < 500;
+        }
+
+        private static int CalculateAgeUsingBirthday(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            bool ageMonthBirthdayNotYetArrived = now.Month < dateOfBirth.Month;
+            bool userBirthdayNotYetArrived = now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day;
+            if (ageMonthBirthdayNotYetArrived || userBirthdayNotYetArrived) age--;
+            return age;
+        }
+
+        private static bool IsEmailValid(string email)
+        {
+            return !email.Contains("@") && !email.Contains(".");
+        }
+
+        private static bool IsLastNameValid(string lastName)
+        {
+            return string.IsNullOrEmpty(lastName);
+        }
+
+        private static bool IsFirstNameValid(string firstName)
+        {
+            return string.IsNullOrEmpty(firstName);
+        }
+    }
+}
